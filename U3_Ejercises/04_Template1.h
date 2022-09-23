a1a0a79aa179eef7a45c1e4b8876a1ab0fa30b70
wicho <luis.ernesto.anaya@upa.edu.mx>
2022-09-23 12:29:40 -0500

    Ajustes

--- U3_Ejercises/04_Template1.h
+++ U3_Ejercises/04_Template1.h
@@ -12,13 +12,13 @@ int maximum (int value1, int value2, int value3){
 }
 
 int minimum (int value1, int value2, int value3){
-    int maximumValue = value1;
+    int minimumValue = value1;
 
-    if (value2 < maximumValue){
-        maximumValue = value2;
+    if (value2 < minimumValue){
+        minimumValue = value2;
     }
-    if (value2 < maximumValue){
-        maximumValue = value3;
+    if (value2 < minimumValue){
+        minimumValue = value3;
     }
-    return maximumValue;
+    return minimumValue;
 }
